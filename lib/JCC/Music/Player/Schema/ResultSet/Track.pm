cb00213445d8fc1f23e09d6a49f1e0c98d629103
Jonathan Cast <jonathanccast@fastmail.fm>
2023-11-23 14:48:03 -0600

    Move threshold calculation into resultset and handle good_genres better

--- lib/JCC/Music/Player/Schema/ResultSet/Track.pm
+++ lib/JCC/Music/Player/Schema/ResultSet/Track.pm
@@ -3,10 +3,19 @@ use Moops;
 use v5.24;
 
 class JCC::Music::Player::Schema::ResultSet::Track extends DBIx::Class::ResultSet {
+    use JCC::Music::Player::Rules qw/ good_genres /;
+
     __PACKAGE__->load_components('Helper::ResultSet::CorrelateRelationship');
     __PACKAGE__->load_components('Helper::ResultSet::IgnoreWantarray');
     __PACKAGE__->load_components('Helper::ResultSet::Me');
     __PACKAGE__->load_components('Helper::ResultSet::Shortcut');
+
+    method apply_threshold($threshold) {
+        $self->search(\[
+            qq{case when genre in (@{[ join(',', map { '?' } good_genres) ]}) then @{[ $self->me('score') ]} >= ? else score >= ? end},
+            good_genres, $threshold / 100 / 2, $threshold / 100,
+        ])
+    }
 
     method nth_row {
         my $target = rand() * $self->result_source->resultset->get_column('score')->sum();
